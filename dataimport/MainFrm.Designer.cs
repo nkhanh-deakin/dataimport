1ee842c65e74b4db986a454f47e6475b1bf14826
nkhanh-deakin <nkhanh@deakin.edu.au>
2016-04-01 00:01:32 +1100

    template

---
+++ dataimport/MainFrm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace dataimport
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMaster = new System.Windows.Forms.ListBox();
+            this.lstSource = new System.Windows.Forms.ListBox();
+            this.txtMaster = new System.Windows.Forms.TextBox();
+            this.btnBrowseMaster = new System.Windows.Forms.Button();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.cbStyle = new System.Windows.Forms.ComboBox();
+            this.txtDelimit = new System.Windows.Forms.TextBox();
+            this.txtSeparator = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnSaveTemplate = new System.Windows.Forms.Button();
+            this.btnLoadTemplate = new System.Windows.Forms.Button();
+            this.btnLoadDefaultTemplate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstMaster
+            // 
+            this.lstMaster.FormattingEnabled = true;
+            this.lstMaster.Location = new System.Drawing.Point(12, 103);
+            this.lstMaster.Name = "lstMaster";
+            this.lstMaster.Size = new System.Drawing.Size(348, 160);
+            this.lstMaster.TabIndex = 0;
+            this.lstMaster.SelectedIndexChanged += new System.EventHandler(this.lstMaster_SelectedIndexChanged);
+            // 
+            // lstSource
+            // 
+            this.lstSource.FormattingEnabled = true;
+            this.lstSource.Location = new System.Drawing.Point(366, 103);
+            this.lstSource.Name = "lstSource";
+            this.lstSource.Size = new System.Drawing.Size(168, 160);
+            this.lstSource.TabIndex = 1;
+            this.lstSource.SelectedIndexChanged += new System.EventHandler(this.lstSource_SelectedIndexChanged);
+            // 
+            // txtMaster
+            // 
+            this.txtMaster.Location = new System.Drawing.Point(71, 15);
+            this.txtMaster.Name = "txtMaster";
+            this.txtMaster.Size = new System.Drawing.Size(365, 20);
+            this.txtMaster.TabIndex = 2;
+            // 
+            // btnBrowseMaster
+            // 
+            this.btnBrowseMaster.Location = new System.Drawing.Point(459, 12);
+            this.btnBrowseMaster.Name = "btnBrowseMaster";
+            this.btnBrowseMaster.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseMaster.TabIndex = 3;
+            this.btnBrowseMaster.Text = "Browse";
+            this.btnBrowseMaster.UseVisualStyleBackColor = true;
+            this.btnBrowseMaster.Click += new System.EventHandler(this.btnBrowseMaster_Click);
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.Location = new System.Drawing.Point(459, 41);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSource.TabIndex = 5;
+            this.btnBrowseSource.Text = "Browse";
+            this.btnBrowseSource.UseVisualStyleBackColor = true;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(71, 44);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(365, 20);
+            this.txtSource.TabIndex = 4;
+            // 
+            // cbStyle
+            // 
+            this.cbStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStyle.FormattingEnabled = true;
+            this.cbStyle.Items.AddRange(new object[] {
+            "Column Style",
+            "Row Mix Style"});
+            this.cbStyle.Location = new System.Drawing.Point(71, 71);
+            this.cbStyle.Name = "cbStyle";
+            this.cbStyle.Size = new System.Drawing.Size(121, 21);
+            this.cbStyle.TabIndex = 6;
+            // 
+            // txtDelimit
+            // 
+            this.txtDelimit.Location = new System.Drawing.Point(215, 72);
+            this.txtDelimit.Name = "txtDelimit";
+            this.txtDelimit.Size = new System.Drawing.Size(100, 20);
+            this.txtDelimit.TabIndex = 7;
+            // 
+            // txtSeparator
+            // 
+            this.txtSeparator.Location = new System.Drawing.Point(346, 71);
+            this.txtSeparator.Name = "txtSeparator";
+            this.txtSeparator.Size = new System.Drawing.Size(100, 20);
+            this.txtSeparator.TabIndex = 8;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(438, 282);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(96, 30);
+            this.btnImport.TabIndex = 9;
+            this.btnImport.Text = "IMPORT";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnSaveTemplate
+            // 
+            this.btnSaveTemplate.Location = new System.Drawing.Point(12, 282);
+            this.btnSaveTemplate.Name = "btnSaveTemplate";
+            this.btnSaveTemplate.Size = new System.Drawing.Size(96, 30);
+            this.btnSaveTemplate.TabIndex = 10;
+            this.btnSaveTemplate.Text = "Save Template";
+            this.btnSaveTemplate.UseVisualStyleBackColor = true;
+            this.btnSaveTemplate.Click += new System.EventHandler(this.btnSaveTemplate_Click);
+            // 
+            // btnLoadTemplate
+            // 
+            this.btnLoadTemplate.Location = new System.Drawing.Point(114, 282);
+            this.btnLoadTemplate.Name = "btnLoadTemplate";
+            this.btnLoadTemplate.Size = new System.Drawing.Size(96, 30);
+            this.btnLoadTemplate.TabIndex = 11;
+            this.btnLoadTemplate.Text = "Load Template";
+            this.btnLoadTemplate.UseVisualStyleBackColor = true;
+            this.btnLoadTemplate.Click += new System.EventHandler(this.btnLoadTemplate_Click);
+            // 
+            // btnLoadDefaultTemplate
+            // 
+            this.btnLoadDefaultTemplate.Location = new System.Drawing.Point(216, 282);
+            this.btnLoadDefaultTemplate.Name = "btnLoadDefaultTemplate";
+            this.btnLoadDefaultTemplate.Size = new System.Drawing.Size(134, 30);
+            this.btnLoadDefaultTemplate.TabIndex = 12;
+            this.btnLoadDefaultTemplate.Text = "Load Default Template";
+            this.btnLoadDefaultTemplate.UseVisualStyleBackColor = true;
+            this.btnLoadDefaultTemplate.Click += new System.EventHandler(this.btnLoadDefaultTemplate_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 324);
+            this.Controls.Add(this.btnLoadDefaultTemplate);
+            this.Controls.Add(this.btnLoadTemplate);
+            this.Controls.Add(this.btnSaveTemplate);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.txtSeparator);
+            this.Controls.Add(this.txtDelimit);
+            this.Controls.Add(this.cbStyle);
+            this.Controls.Add(this.btnBrowseSource);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnBrowseMaster);
+            this.Controls.Add(this.txtMaster);
+            this.Controls.Add(this.lstSource);
+            this.Controls.Add(this.lstMaster);
+            this.Name = "MainFrm";
+            this.Text = "Data Import";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMaster;
+        private System.Windows.Forms.ListBox lstSource;
+        private System.Windows.Forms.TextBox txtMaster;
+        private System.Windows.Forms.Button btnBrowseMaster;
+        private System.Windows.Forms.Button btnBrowseSource;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.ComboBox cbStyle;
+        private System.Windows.Forms.TextBox txtDelimit;
+        private System.Windows.Forms.TextBox txtSeparator;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSaveTemplate;
+        private System.Windows.Forms.Button btnLoadTemplate;
+        private System.Windows.Forms.Button btnLoadDefaultTemplate;
+    }
+}
+
---
+++ dataimport/MainFrm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpreadsheetLight;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace dataimport
+{
+    public partial class MainFrm : Form
+    {
+        string default_template_file = "template.txt";
+        List<string> master = new List<string>();
+        List<int> master_source = new List<int>();
+        List<string> source = new List<string>();
+        XmlSerializer s = new XmlSerializer(typeof(List<int>));
+        SLDocument master_doc = null;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+            cbStyle.SelectedIndex = 0;
+            txtDelimit.Text = ",\t;";
+        }
+
+        private string selectFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+                return dlg.FileName;
+            return null;
+        }
+
+        private void btnBrowseMaster_Click(object sender, EventArgs e)
+        {
+            string filename = selectFile();
+            if (filename == null)
+                return;
+            txtMaster.Text = filename;
+            master_doc = new SLDocument(filename);
+            master_doc.SelectWorksheet("results");
+
+            int i = 1;
+
+            lstMaster.Items.Clear();
+            master.Clear();
+            master_source.Clear();
+            List<int> t2 = new List<int>();
+            while (master_doc.GetCellValueAsString(1, i).Trim() != "")
+            {
+                string st = master_doc.GetCellValueAsString(1, i).Trim();
+                lstMaster.Items.Add(st + " << ");
+                master.Add(st);
+                master_source.Add(-1);
+                i++;
+            }
+        }
+
+        StreamReader source_reader;
+        private void readSourceFile()
+        {
+            source_reader = new StreamReader(txtSource.Text);
+            string line = source_reader.ReadLine();
+            if (cbStyle.SelectedIndex < 0)
+                return;
+
+            lstSource.Items.Clear();
+            lstSource.Items.Add("(none)");
+            source.Clear(); 
+            if (cbStyle.SelectedIndex == 0)
+            {
+                source.AddRange(line.Split(txtDelimit.Text.ToCharArray()));
+                for (int i = 0; i < source.Count; i++)
+                {
+                    lstSource.Items.Add(source[i]);
+                }
+            }
+        }
+
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            string filename = selectFile();
+            if (filename == null)
+                return;
+            txtSource.Text = filename;
+
+            readSourceFile();
+        }
+
+        private void lstMaster_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lstMaster.SelectedIndex;
+            if (index < 0)
+                return;
+            if (lstSource.Items.Count <= 0)
+                return;
+            lstSource.SelectedIndex = master_source[index] + 1;
+        }
+
+        private void lstSource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index_source = lstSource.SelectedIndex;
+            int index_master = lstMaster.SelectedIndex;
+            if ((index_master < 0) || (index_source < 0))
+                return;
+            master_source[index_master] = index_source - 1;
+            lstMaster.Items[index_master] = master[index_master] + " << "
+               + ((index_source <= 0) ? "" : source[index_source - 1]);
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            int iRow = 1;
+            while (master_doc.GetCellValueAsString(iRow, 1).Trim() != "")
+                iRow++;
+            if (cbStyle.SelectedIndex == 0)
+            {
+                while (!source_reader.EndOfStream)
+                {
+                    string line = source_reader.ReadLine();
+                    string[] data = line.Split(txtDelimit.Text.Trim().ToCharArray());
+                    for (int i = 0; i < master.Count; i++)
+                    {
+                        int index_source = master_source[i];
+                        string value = (index_source < 0) ? "" : data[index_source];
+                        
+                        master_doc.SetCellValueNumeric(iRow, i + 1, value);
+                    }
+                    iRow++;
+                }
+            }
+            master_doc.Save();
+        }
+
+        private void btnSaveTemplate_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+                s.Serialize(new StreamWriter(dlg.FileName), master_source);
+        }
+
+        private void btnLoadTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                master_source = (List<int>)s.Deserialize(new StreamReader(dlg.FileName));
+            }
+        }
+
+        private void btnLoadDefaultTemplate_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists(default_template_file))
+                return;
+            master_source = (List<int>)s.Deserialize(new StreamReader(default_template_file));
+        }
+    }
+}
